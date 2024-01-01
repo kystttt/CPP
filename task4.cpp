f94baefb1e3f05d4d908666d086fa43025528627
Leonid <146662933+kystttt@users.noreply.github.com>
2024-01-01 21:15:03 +0500

    task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	// Создание файла
+	ofstream MyFile("source.txt");
+	MyFile << "aaaaabbbccaaaaaahbjkkkkkkkkkkl";
+	MyFile.close();
+
+	string myText;
+	ifstream MyReadFile("source.txt");
+	
+	
+	int  answer = 0, temp = 1;
+	
+
+	// построчное чтение файла
+	while (getline(MyReadFile, myText)) {
+		int Textlen = myText.length();
+		for (int i = 0; i < Textlen - 1; i++) {
+			if (myText[i] == myText[i + 1]) {
+				temp++;
+				answer = max(answer, temp);
+			}
+			else {
+				temp = 1;
+			} 
+			cout << myText[i] << endl;
+		}
+	}
+	cout << answer << endl;
+	MyReadFile.close();
+	return 0;
+}
