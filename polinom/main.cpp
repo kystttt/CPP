78cc653d137bb657d486b4e918c4e82e1ec83d8f
Leonid <146662933+kystttt@users.noreply.github.com>
2024-04-01 22:01:24 +0500

    Create main.cpp

---
+++ polinom/main.cpp
@@ -0,0 +1,21 @@
+#include "Polinom.h"
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	
+	Polinom f;
+	Polinom g;
+	f >> cin;
+	/*g >> cin;
+	f.print();
+	g.print();
+	f + g;*/
+	f.print();
+	return 0;
+}
